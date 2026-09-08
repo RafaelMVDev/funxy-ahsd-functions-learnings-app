6c8db932211c90a8bc7e9396c17065ff37e8dccb
RafaelMVDev <rafaelmoralesvillela@gmail.com>
2026-09-07 21:10:14 -0300

    feat: usuario model

--- src/main/java/br/edu/ifsp/funxyApp/model/Usuario.java
+++ src/main/java/br/edu/ifsp/funxyApp/model/Usuario.java
@@ -1,41 +1,99 @@
 package br.edu.ifsp.funxyApp.model;
+import jakarta.persistence.*;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Column;
+import java.math.BigInteger;
 
 @Entity
+@Table(name = "usuario")
 public class Usuario {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false, length = 100)
+    @Column(name="email")
+    private String email;
+
+    @Column(name="nome")
     private String nome;
 
-    @Column(nullable = false, length = 100)
-    private String curso;
+    @Column(name="senha_hash")
+    private String senhaHash;
 
-    @Column(nullable = false, unique = true)
-    private String prontuario;
+    @Column(name="sequencia")
+    private Integer sequencia;
+
+    @Column(name="moedas")
+    private Long moedas;
+
+    @Column(name="xp")
+    private Long xp;
+
 
     public Usuario() {}
 
-    public Usuario(String nome, String prontuario, String curso) {
+    public Usuario(String nome, String email, String senhaHash, Integer sequencia, Long moedas, Long xp) {
         this.nome = nome;
-        this.prontuario = prontuario;
-        this.curso = curso;
+        this.email = email;
+        this.senhaHash = senhaHash;
+        this.sequencia = sequencia;
+        this.moedas = moedas;
+        this.xp = xp;
     }
 
-    public Long getId() { return id; }
-    public void setId(Long id) { this.id = id; }
-    public String getNome() { return nome; }
-    public void setNome(String nome) { this.nome = nome; }
-    public String getProntuario() { return prontuario; }
-    public void setProntuario(String prontuario) { this.prontuario = prontuario; }
-    public String getCurso() { return curso; }
-    public void setCurso(String curso) { this.curso = curso; }
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSenhaHash() {
+        return senhaHash;
+    }
+
+    public void setSenhaHash(String senhaHash) {
+        this.senhaHash = senhaHash;
+    }
+
+    public Integer getSequencia() {
+        return sequencia;
+    }
+
+    public void setSequencia(Integer sequencia) {
+        this.sequencia = sequencia;
+    }
+
+    public Long getMoedas() {
+        return moedas;
+    }
+
+    public void setMoedas(Long moedas) {
+        this.moedas = moedas;
+    }
+
+    public Long getXp() {
+        return xp;
+    }
+
+    public void setXp(Long xp) {
+        this.xp = xp;
+    }
 }
